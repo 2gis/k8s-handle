c1e4828db39e507171c5a1ca81cabbee96278841
furiousassault <winterspear@yandex.ru>
2018-09-04 12:02:57 +0700

    I-7 - clean up argparse subparsers.

--- k8s-handle.py
+++ k8s-handle.py
@@ -20,7 +20,8 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=settings.LOG_LEVEL, format=settings.LOG_FORMAT, datefmt=settings.LOG_DATE_FORMAT)
 
 parser = argparse.ArgumentParser(description='CLI utility generate k8s resources by templates and apply it to cluster')
-subparsers = parser.add_subparsers()
+subparsers = parser.add_subparsers(dest="command")
+subparsers.required = True
 
 deploy_parser = subparsers.add_parser('deploy', help='Sub command for deploy')
 deploy_parser.add_argument('-s', '--section', required=True, type=str, help='Section to deploy from config file')
@@ -35,8 +36,6 @@ deploy_parser.add_argument('--strict', action='store_true', required=False,
                            help='Check existence of all env variables in config.yaml and stop deploy if var is not set')
 deploy_parser.add_argument('--use-kubeconfig', action='store_true', required=False, help='Try to use kube config')
 
-deploy_parser.set_defaults(command='deploy')
-
 destroy_parser = subparsers.add_parser('destroy', help='Sub command for destroy app')
 destroy_parser.add_argument('-s', '--section', required=True, type=str, help='Section to destroy from config file')
 destroy_parser.add_argument('-c', '--config', type=str, required=False, help='Config file, default: config.yaml')
@@ -48,8 +47,6 @@ destroy_parser.add_argument('--tries', type=int, required=False, default=360,
                             help='Count of tries to check destruction status')
 destroy_parser.add_argument('--retry-delay', type=int, required=False, default=5, help='Sleep between tries in seconds')
 destroy_parser.add_argument('--use-kubeconfig', action='store_true', required=False, help='Try to use kube config')
-
-destroy_parser.set_defaults(command='destroy')
 
 
 def main():
@@ -68,10 +65,6 @@ def main():
 
     if 'strict' in args:
         settings.GET_ENVIRON_STRICT = args.strict
-
-    if 'command' not in args:
-        parser.print_help()
-        sys.exit(1)
 
     try:
         context = config.context_load(args.section)
